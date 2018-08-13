7dba03fba85daef1e114e4920d5ac7f3f371dfcb
Matthew Mage <sciguymjm@gmail.com>
2018-08-13 10:37:33 -0400

    add top level init

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from json_parser.game import Game
+from json_parser.player import Player
+from json_parser.goal import Goal
+
+from analysis.saltie_game.saltie_game import SaltieGame
+from analysis.saltie_game.metadata.ApiGame import ApiGame
+from decompile_replays import decompile_replay
