709f3043205e512b848549c25755160b75c668be
Sciguymjm <sciguymjm@gmail.com>
2018-08-25 20:21:17 -0400

    made proto batch file more robust

--- proto.bat
+++ proto.bat
@@ -1,7 +1,7 @@
 echo "Creating proto files"
 
-%CD%\replay_analysis\generated\protoc.exe --proto_path=%CD% --python_out=%CD%\replay_analysis\generated %CD%\**\*.proto
-%CD%\replay_analysis\generated\protoc.exe --proto_path=%CD% --python_out=%CD%\replay_analysis\generated %CD%\api\**\*.proto
+for /f %%i in ('dir /a:d /s /b api') do %CD%\replay_analysis\generated\protoc.exe --proto_path=%CD% --python_out=%CD%\replay_analysis\generated %%i\*.proto
+%CD%\replay_analysis\generated\protoc.exe --proto_path=%CD% --python_out=%CD%\replay_analysis\generated %CD%\api\*.proto
 
 echo "Proto files are created"
 
