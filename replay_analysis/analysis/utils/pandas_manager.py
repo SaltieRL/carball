22ef303bd38334573c5ed838ba094ee6caea2091
Matthew Mage <sciguymjm@gmail.com>
2018-08-28 16:32:54 -0400

    remove data frame extension

--- replay_analysis/analysis/utils/pandas_manager.py
+++ replay_analysis/analysis/utils/pandas_manager.py
@@ -1,31 +1,15 @@
 import io
 import logging
 
-import pandas
 from pandas import DataFrame
 
 from replay_analysis.analysis.utils.numpy_manager import write_array_to_file, read_array_from_file
-from ...generated.api import game_pb2
-from ...generated.api.stats import data_frame_pb2
 
 
 logger = logging.getLogger(__name__)
 
 
 class PandasManager:
-    @staticmethod
-    def add_pandas(protobuf_game: game_pb2.Game, data_frame: pandas.DataFrame):
-        hdf5_bytes = PandasManager.safe_write_pandas_to_memory(data_frame, field_name="game_frames")
-        if hdf5_bytes is not None:
-            protobuf_game.Extensions[data_frame_pb2.data_frame] = hdf5_bytes
-
-    @staticmethod
-    def get_pandas(protobuf_game: game_pb2.Game):
-        game_frames = None
-        if protobuf_game.HasExtension("data_frame"):
-            game_frames = PandasManager.safe_read_pandas_to_memory(protobuf_game.Extensions[data_frame_pb2.data_frame],
-                                                                   field_name="game_frames")
-        return game_frames
 
     @staticmethod
     def safe_write_pandas_to_memory(df, field_name=""):
