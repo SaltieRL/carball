638b3cab1ed62cff6177b13624d67b91dc2fdd75
Druthyn <nutkin473@gmail.com>
2018-09-13 20:32:03 +0100

    Implementing recommended changes

--- carball/analysis/stats/controls/controls.py
+++ carball/analysis/stats/controls/controls.py
@@ -8,30 +8,23 @@ from ....generated.api.player_pb2 import Player
 from ....generated.api.stats.player_stats_pb2 import PlayerStats
 from ....json_parser.game import Game
 
-
 class ControlsStat(BaseStat):
 
     def calculate_player_stat(self, player_stat_map: Dict[str, PlayerStats], game: Game, proto_game: game_pb2.Game,
                               player_map: Dict[str, Player], data_frame: pd.DataFrame):
         for player_key, stats in player_map.items():
             player_name = player_map[player_key].name
-            steer_values = data_frame[player_name]['steer'].tolist()
-            throttle_values = data_frame[player_name]['throttle'].tolist()
-            steering_percentage = self.get_analogue_percentage(steer_values)
-            throttle_percentage = self.get_analogue_percentage(throttle_values)
-            is_keyboard = (steering_percentage == 0) and (throttle_percentage == 0)
+
+            steering_percentage = self.get_analogue_percentage("steer", data_frame, player_name)
+            throttle_percentage = self.get_analogue_percentage("throttle", data_frame, player_name)
+            is_keyboard = bool((steering_percentage == 0) and (throttle_percentage == 0))
 
             controller_stats = player_stat_map[player_key].controller
             controller_stats.is_keyboard = is_keyboard
             controller_stats.analogue_steering_input_percent = throttle_percentage
             controller_stats.analogue_throttle_input_percent = steering_percentage
 
-
-    def get_analogue_percentage(self, values: list):
-        total_frames = values.__len__()
-        count = 0
-        for value in values:
-            if value not in (0, 128, 255):
-                if not pd.isnull(value):
-                    count = count + 1
-        return (count * 100) / total_frames
+    def get_analogue_percentage(self, column: str, data_frame: pd.DataFrame, player_name: str):
+        total_frames = len(data_frame[player_name][column])
+        count = (data_frame[player_name][column] == 0).sum() + (data_frame[player_name][column] == 128).sum()  + (data_frame[player_name][column] == 255).sum() + data_frame[player_name][column].isna().sum()
+        return 100-((count * 100) / total_frames)
