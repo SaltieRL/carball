26a79e5bf105124e0c7df2e9ee2b442130fae3ee
dtracers <dtracers@gmail.com>
2018-09-23 14:36:36 -0600

    fix imports

--- carball/tests/analysis_test.py
+++ carball/tests/analysis_test.py
@@ -6,9 +6,9 @@ import subprocess
 import traceback
 from io import BytesIO
 
-from carball.analysis.utils.pandas_manager import PandasManager
 from google.protobuf.json_format import MessageToJson
 
+from carball.analysis.utils.pandas_manager import PandasManager
 from carball.controls.controls import ControlsCreator
 from carball.decompile_replays import analyze_replay_file
 from carball.json_parser.sanity_check.sanity_check import SanityChecker
