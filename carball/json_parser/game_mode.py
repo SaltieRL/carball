c931048a3f08d4ddd73ac2111baef9330fad1eca
Prettner <s.prettner@oth-aw.de>
2018-09-25 02:13:12 +0200

    Added metadata mutators Added Enums Playlist, GameMode

---
+++ carball/json_parser/game_mode.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class GameMode(Enum):
+    solo_standard = 1
+    duel = 2
+    duos = 3
+    standard = 4
+    chaos = 5
+    hoops = 6
+    dropshot = 7
+    hockey = 8
+    rumble = 9
+    custom = 10
