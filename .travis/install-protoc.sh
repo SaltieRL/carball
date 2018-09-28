62c8302490e02105cc3af3e651c4baadb88dfee6
dtracers <dtracers@gmail.com>
2018-09-28 13:57:55 -0600

    Automated testing (#67)
    * fixing relative imports for tests
    
    * creating the protobuf for tests
    
    * adding relative imports
    
    * working on crashing if proto file is invalid
    
    * fixing install
    
    * adding protobuf files
    
    * splitting up install
    
    * working on installing protobuf 2.6.1
    
    * working on installing protobuf
    
    * working on making protoc be equivalent
    
    * logging
    
    * more just random tests
    
    * copying over proto file
    
    * stuff
    
    * test
    
    * install protobuf
    
    * oops
    
    * config?
    
    * sudo all the things
    
    * maybe more luck this time
    
    * fix permission errors
    
    * add all the libraries?
    
    * idk trying to make things work
    
    * updating stuff
    
    * more logs
    
    * travis is dumb sometimes
    
    * this  is dumb
    
    * moved things around
    
    * this is way too dumb
    
    * forgot something
    
    * test2
    
    * yes
    
    * install protoc script
    
    * chmod
    
    * adjust script
    
    * adjust script
    
    * adjust script
    
    * sudo
    
    * remove protoc
    
    * fix script
    
    * mhmm
    
    * plz
    
    * more debugging
    
    * final fix?
    
    * apparently protoc exiting with code 1 kills the build
    
    * ignore protobuf
    
    * fix running test

---
+++ .travis/install-protoc.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -eu
+protoc_version=$1
+wget https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protobuf-${protoc_version}.zip
+unzip protobuf-${protoc_version}.zip
+cd protobuf-${protoc_version}
+./configure
+make
+./src/protoc --version
+pwd
+ls -lh
+cd ..
+pwd
+ls -lh
+cp ./protobuf-${protoc_version}/src/protoc carball/generated
