8b086b9ad109ec438606d84db394c8ffda7c34a9
Matthew Mage <sciguymjm@gmail.com>
2018-09-28 11:56:35 -0400

    fix script

--- .travis/install-protoc.sh
+++ .travis/install-protoc.sh
@@ -1,21 +1,10 @@
 #!/bin/sh -eu
 protoc_version=$1
-if test -z "${protoc_version}"; then
-	echo "Usage: .travis/install-protoc.sh protoc-version"
-	exit 1
-fi
-if [ "`$HOME/local/bin/protoc --version 2>/dev/null | cut -d' ' -f 2`" != ${protoc_version} ]; then
-	rm -rf $HOME/local/bin $HOME/local/include
-
-	mkdir -p $HOME/tmp $HOME/local
-	cd $HOME/tmp
-	wget https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protobuf-${protoc_version}.zip
-	unzip protobuf-${protoc_version}.zip
-	cd protobuf-${protoc_version}
-	./configure
-	make
-	sudo make install
-fi
-
-echo \$ protoc --version
-protoc --version
+wget https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protobuf-${protoc_version}.zip
+unzip protobuf-${protoc_version}.zip
+cd protobuf-${protoc_version}
+./configure
+make
+./src/protoc --version
+cd ../..
+cp ./.travis/protobuf-${protoc_version}/src/protoc carball/generated
