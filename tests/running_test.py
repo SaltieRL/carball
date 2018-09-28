8cae3e47010b680460c7be36a8764c1973044dea
Matthew Mage <sciguymjm@gmail.com>
2018-09-28 15:47:49 -0400

    fix running test

--- tests/running_test.py
+++ tests/running_test.py
@@ -3,9 +3,3 @@ from tests.utils import download_replay_discord, save_locally
 
 def get_replay_list():
     return ['https://cdn.discordapp.com/attachments/493849514680254468/493880540802449462/UnicodeEncodeError.replay']
-
-
-
-
-def test_replay_runs():
-
