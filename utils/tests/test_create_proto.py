e72b60d53ab548cc3329a121f51665eedac178ad
Ken Colton <kcolton@gmail.com>
2018-09-29 14:20:07 -0400

    Chore/allow protoc path from env var (#72)
    * Allow protoc path from env var so don't need to add OSX bin to repo
    
    * Add .env to gitignore for local env vars
    
    * Add unit tests for get_proto
    
    * Absolute import to make travis more happy and test easier to move

---
+++ utils/tests/test_create_proto.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+from unittest.mock import patch
+
+from utils.create_proto import get_proto
+
+
+class GetProtoTests(unittest.TestCase):
+    @patch.dict(os.environ, {'PROTOC_PATH': '/foo/bar/protoc'})
+    def test_with_env_var(self):
+        protoc_path = get_proto()
+        self.assertEqual(protoc_path, '/foo/bar/protoc')
+
+    @patch('utils.create_proto.is_windows', return_value=True)
+    def test_windows(self, is_windows):
+        self.assertIsNone(os.getenv('PROTOC_PATH'))
+        self.assertTrue(is_windows())
+
+        protoc_path = get_proto()
+        self.assertTrue(protoc_path.endswith('protoc.exe'))
+
+    @patch('utils.create_proto.is_windows', return_value=False)
+    def test_is_not_windows(self, is_windows):
+        self.assertIsNone(os.getenv('PROTOC_PATH'))
+        self.assertFalse(is_windows())
+
+        protoc_path = get_proto()
+        self.assertTrue(protoc_path.endswith('protoc'))
