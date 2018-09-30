63565c6652b588cde32b830e86212d7af929ed61
dtracers <dtracers@gmail.com>
2018-09-30 15:31:08 -0600

    added more replays

--- carball/tests/utils.py
+++ carball/tests/utils.py
@@ -21,19 +21,21 @@ def save_locally(replay_object):
     return file_path
 
 
-def run_tests_on_list(unit_test_func: Callable, replay_list=None):
+def run_tests_on_list(unit_test_func: Callable, replay_list=None, answers=None):
     if replay_list is None:
         replay_list = get_full_replay_list()
 
-    for replay_url in replay_list:
-        run_replay(replay_url, unit_test_func)
+    for index in range(len(replay_list)):
+        answer = answers[index] if answers is not None and index < len(answers) else None
+        run_replay(replay_list[index], unit_test_func, answer=answer)
 
 
-def run_replay(url, func: Callable):
+def run_replay(url, unit_test_func: Callable, answer=None):
     """
     Runs the replay with the file downloaded locally then deletes the file.
     :param url:
-    :param func:
+    :param unit_test_func:
+    :param answer: data that can be passed to the replay to help judge it
     :return:
     """
 
@@ -42,21 +44,27 @@ def run_replay(url, func: Callable):
 
     fd, file_path = tempfile.mkstemp()
     os.close(fd)
-    func(file, file_path)
+    if answer is not None:
+        unit_test_func(file, file_path, answer)
+    else:
+        unit_test_func(file, file_path)
     os.remove(file)
     os.remove(file_path)
 
 
-def run_analysis_test_on_replay(unit_test_func: Callable, replay_list=None):
+def run_analysis_test_on_replay(unit_test_func: Callable, replay_list=None, answers=None):
     """
     :param unit_test_func: Called with an AnalysisManager
     :param replay_list: list of replay urls
     :return:
     """
-    def wrapper(replay_file_path, json_file_path):
+    def wrapper(replay_file_path, json_file_path, answer=None):
         analysis_manager = analyze_replay_file(replay_file_path, json_file_path)
-        unit_test_func(analysis_manager)
-    run_tests_on_list(wrapper, replay_list)
+        if answer is not None:
+            unit_test_func(analysis_manager, answer)
+        else:
+            unit_test_func(analysis_manager)
+    run_tests_on_list(wrapper, replay_list, answers=answers)
 
 
 def get_full_replay_list():
@@ -67,7 +75,7 @@ def get_full_replay_list():
     return ['https://cdn.discordapp.com/attachments/493849514680254468/493880540802449462/UnicodeEncodeError.replay']
 
 
-def get_specific_replays():
+def get_raw_replays():
     """
     Partial replays for specific values.
     :return:
@@ -81,16 +89,49 @@ def get_specific_replays():
         "1_EPIC_SAVE": ["https://cdn.discordapp.com/attachments/493849514680254468/495735191537713153/1_EPIC_SAVE.replay"],
         "1_JUMP": ["https://cdn.discordapp.com/attachments/493849514680254468/495735203323576321/1_JUMP.replay"],
         "1_NORMAL_SAVE": ["https://cdn.discordapp.com/attachments/493849514680254468/495735215767945234/1_NORMAL_SAVE.replay"],
+        # Boost
         "12_BOOST_PAD_0_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/495735228422291456/12_BOOST_PAD_0_USED.replay"],
         "12_BOOST_PAD_45_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/495735240321662986/12_BOOST_PAD_45_USED.replay"],
         "100_BOOST_PAD_0_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/495735252233224193/100_BOOST_PAD_0_USED.replay"],
         "100_BOST_PAD_100_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/495735264262488065/100_BOOST_PAD_100_USED.replay"],
         "NO_BOOST_PAD_0_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/495735276338020372/NO_BOOST_PAD_0_USED.replay"],
         "NO_BOOST_PAD_33_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/495735288254169109/NO_BOOST_PAD_33_USED.replay"],
+        "12_AND_100_BOOST_PADS_0_USED": ["https://cdn.discordapp.com/attachments/493849514680254468/496071113768697859/12_AND_100_BOOST_PADS_0_USED.replay"],
+        # Kickoffs
         "STRAIGHT_KICKOFF_GOAL": ["https://cdn.discordapp.com/attachments/493849514680254468/495735301604376576/Straight_Kickoff_Goal.replay"],
         "KICKOFF_NO_TOUCH": ["https://cdn.discordapp.com/attachments/493849514680254468/496034430943756289/NO_KICKOFF.replay"],
         "3_KICKOFFS": ["https://cdn.discordapp.com/attachments/493849514680254468/496034443442782208/3_KICKOFFS_4_SHOTS.replay"],
-        "4_SHOTS": ["https://cdn.discordapp.com/attachments/493849514680254468/496034443442782208/3_KICKOFFS_4_SHOTS.replay"]
+        # hits
+        "4_SHOTS": ["https://cdn.discordapp.com/attachments/493849514680254468/496034443442782208/3_KICKOFFS_4_SHOTS.replay"],
+        "MID_AIR_PASS": ["https://cdn.discordapp.com/attachments/493849514680254468/495887162928267314/MID_AIR_PASS_GOAL.replay"],
+        "HIGH_AIR_PASS": ["https://cdn.discordapp.com/attachments/493849514680254468/495887164425633802/HIGH_AIR_PASS_GOAL.replay"],
+        "GROUND_PASS": ["https://cdn.discordapp.com/attachments/493849514680254468/495887165570678794/GROUNDED_PASS_GOAL.replay"],
+        "PINCH_GROUND": ["https://cdn.discordapp.com/attachments/493849514680254468/495887167932071947/PINCH_GROUNDED_GOAL.replay"],
     }
 
+
+def get_specific_replays():
+    raw_map = get_raw_replays()
+    return {
+        # BOOSTS
+        "0_BOOST_COLLECTED": raw_map["NO_BOOST_PAD_0_USED"] + raw_map["NO_BOOST_PAD_33_USED"] + raw_map["KICKOFF_NO_TOUCH"],
+        "1_SMALL_PAD": raw_map["12_BOOST_PAD_0_USED"] + raw_map["12_BOOST_PAD_45_USED"],
+        "1_LARGE_PAD": raw_map["100_BOOST_PAD_0_USED"] + raw_map["100_BOST_PAD_100_USED"],
+        "0_BOOST_USED": raw_map["12_BOOST_PAD_0_USED"] + raw_map["100_BOOST_PAD_0_USED"] + raw_map["NO_BOOST_PAD_0_USED"] + raw_map["KICKOFF_NO_TOUCH"],
+        "BOOST_USED": raw_map["12_BOOST_PAD_45_USED"] + raw_map["100_BOST_PAD_100_USED"] + raw_map["NO_BOOST_PAD_33_USED"],  # [45, 100, 33]
+
+        # HITS
+        "HITS": raw_map["4_SHOTS"] + raw_map["12_BOOST_PAD_45_USED"],
+        "SHOTS": raw_map["4_SHOTS"] + raw_map["12_BOOST_PAD_45_USED"],
+        "PASSES": raw_map["MID_AIR_PASS"] + raw_map["HIGH_AIR_PASS"]  + raw_map["HIGH_AIR_PASS"],
+    }
+
+def get_specific_answers():
+    specific_replays = get_specific_replays()
+    return {
+        "0_BOOST_USED": [0] * len(specific_replays["0_BOOST_USED"]),
+        "BOOST_USED": [45, 100, 33],
+        "HITS": [4, 1],
+        "SHOTS": [4, 1]
+    }
 
