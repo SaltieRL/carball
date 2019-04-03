93a1811778241c4723c67fa38c66290db7b91f31
dtracers <dtracers@gmail.com>
2019-04-02 22:39:18 -0600

    modifications to carries for cutting them off

--- carball/analysis/events/carry_detection.py
+++ carball/analysis/events/carry_detection.py
@@ -9,6 +9,7 @@ from carball.generated.api.player_pb2 import Player
 
 logger = logging.getLogger(__name__)
 
+
 class CarryData:
     def __init__(self, carry_frames: pd.DataFrame, start_frames: List[int], end_frames: List[int]):
         self.carry_frames = carry_frames
@@ -49,9 +50,71 @@ class CarryDetection:
         # look up hits.
         # any hits by the same player within a continous set of valid frames should count as carrys
 
+    def correct_carries(self, carry_data: CarryData, player_carry_data: CarryData,
+                        player: Player, proto_game: game_pb2.Game) -> CarryData:
+        """
+        This modifies carries to correct for certain situations at a better granularity.
+        """
+
+        self.merge_carries(carry_data, player_carry_data)
+
+        self.shorten_carries(player_carry_data, player, proto_game)
+
+        # Look for the first carry frame and find all hits before it by the same player
+
+        return player_carry_data
+
+    def shorten_carries(self, player_carry_data: CarryData, player: Player, proto_game: game_pb2.Game):
+        """
+        Shortens carries in cases where the carry was ended by a player stealing the ball.
+        """
+        start_frames = player_carry_data.start_frames
+        end_frames = player_carry_data.end_frames
+
+        hit_list = proto_game.game_stats.hits
+
+        def valid_hit_number(hit_index, carry_index):
+            return hit_list[hit_index].frame_number < end_frames[carry_index] and hit_index < len(hit_list) - 1
+
+        hit_number = 0
+        for carry_index in range(len(start_frames)):
+            while hit_number < len(hit_list) - 1:
+                if hit_list[hit_number].frame_number < start_frames[carry_index]:
+                    hit_number += 1
+                    continue
+                if hit_list[hit_number].frame_number >= end_frames[carry_index]:
+                    break
+
+                invalid_hit = None
+                last_player_hit = None
+                while valid_hit_number(hit_number, carry_index):
+                    if hit_list[hit_number].player_id.id != player.id.id:
+                        # We need to potentially change how the carry occurred
+                        if invalid_hit is None:
+                            invalid_hit = hit_list[hit_number]
+                        while hit_list[hit_number].player_id.id != player.id.id and valid_hit_number(hit_number,
+                                                                                                     carry_index):
+                            hit_number += 1
+                        if hit_list[hit_number].frame_number >= end_frames[carry_index]:
+                            # The player never hits it again, end the carry early.
+                            player_carry_data.end_frames[carry_index] = invalid_hit.frame_number
+                    else:
+                        hit_number += 1
+                    if valid_hit_number(hit_number - 1, carry_index) and hit_list[hit_number - 1].player_id.id == player.id.id:
+                        last_player_hit = hit_list[hit_number - 1]
+                if last_player_hit is None:
+                    logger.error('The player never hit the ball during the "carry"')
+                    end_frames[carry_index] = start_frames[carry_index]
+                else:
+                    last_player_hit
+
+            if hit_number >= len(hit_list) - 1:
+                break
+
     def merge_carries(self, carry_data: CarryData, player_carry_data: CarryData):
         """
-        Merges separated carries that are still within the valid carry set.
+        Merges separated player carries that are still within the greater valid carry set.
+        This modifies player_carry_data
         """
         start_frames = player_carry_data.start_frames
         end_frames = player_carry_data.end_frames
@@ -92,63 +155,9 @@ class CarryDetection:
         player_carry_data.start_frames = merged_start
         player_carry_data.end_frames = merged_end
 
-    def correct_carries(self, carry_data: CarryData, player_carry_data: CarryData,
-                        player: Player, proto_game: game_pb2.Game) -> CarryData:
-        """
-        This modifies carries to correct for certain situations at a better granularity.
-        For examples cases where one carry was instead counted as 2.
-        cases where the carry should stop counting.
-        """
-
-        self.merge_carries(carry_data, player_carry_data)
-        start_frames = player_carry_data.start_frames
-        end_frames = player_carry_data.end_frames
-
-        hit_list = proto_game.game_stats.hits
-
-        def valid_hit_number(hit_index, carry_index):
-            return hit_list[hit_index].frame_number < end_frames[carry_index] and hit_index < len(hit_list) - 1
-
-        hit_number = 0
-        for carry_index in range(len(start_frames)):
-            while hit_number < len(hit_list) - 1:
-                if hit_list[hit_number].frame_number < start_frames[carry_index]:
-                    hit_number += 1
-                    continue
-                if hit_list[hit_number].frame_number >= end_frames[carry_index]:
-                    break
-
-                while valid_hit_number(hit_number, carry_index):
-                    if hit_list[hit_number].player_id.id != player.id.id:
-                        # We need to potentially change how the carry occurred
-                        invalid_hit = hit_list[hit_number]
-                        while hit_list[hit_number].player_id.id != player.id.id and valid_hit_number(hit_number,
-                                                                                                     carry_index):
-                            hit_number += 1
-                        if hit_list[hit_number].frame_number >= end_frames[carry_index]:
-                            # The player never hits it again, end the carry early.
-                            player_carry_data.end_frames[carry_index] = invalid_hit.frame_number
-                    else:
-                        hit_number += 1
-
-            if hit_number >= len(hit_list) - 1:
-                break
-        # Look for the first carry frame and find all hits before it by the same player
-
-        return player_carry_data
-
-    def get_straight_line_distance(self, player_frames: pd.DataFrame, start: int, end: int) -> pd.DataFrame:
-        x1 = player_frames.pos_x[start]
-        y1 = player_frames.pos_y[start]
-
-        x2 = player_frames.pos_x[end]
-        y2 = player_frames.pos_y[end]
-
-        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-
     def creat_start_end_frames(self, carry_frames: pd.DataFrame) -> Tuple[List[int], List[int]]:
         """
-        Returns the first and last frame of each carry
+        Returns the first and last frame of each potential carry
         """
         carry_frame_index = carry_frames.index
         shifted = carry_frame_index.to_series().shift(fill_value=0)
@@ -189,5 +198,19 @@ class CarryDetection:
             carry_stats.variance_z_distance = z_distance.var()
             carry_stats.variance_ball_z_velocity = carry_frames.ball.vel_z.var()
             carry_stats.average_carry_speed = ((player_frames.vel_x ** 2 + player_frames.vel_y ** 2) ** 0.5).mean()
-            carry_stats.distance_along_path = (player_frames.x[start_frames[i]:end_frames[i]].diff()**2 +
-                                               player_frames.y[start_frames[i]:end_frames[i]].diff()**2).sqrt()
+            carry_stats.distance_along_path = self.get_distance_along_path(player_frames,
+                                                                           start_frames[i], end_frames[i])
+
+    def get_distance_along_path(self, player_frames: pd.DataFrame, start_frame: int, end_frame: int):
+        pos_x = player_frames.pos_x.loc[start_frame:end_frame].diff()
+        pos_y = player_frames.pos_y.loc[start_frame:end_frame].diff()
+        return ((pos_x ** 2 + pos_y ** 2) ** 0.5).sum()
+
+    def get_straight_line_distance(self, player_frames: pd.DataFrame, start: int, end: int) -> pd.DataFrame:
+        x1 = player_frames.pos_x[start]
+        y1 = player_frames.pos_y[start]
+
+        x2 = player_frames.pos_x[end]
+        y2 = player_frames.pos_y[end]
+
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
