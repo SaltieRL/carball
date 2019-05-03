fbcc7a4007bed0186b4148bd5ddb3661efb2eda3
dtracers <dtracers@gmail.com>
2019-05-03 08:54:55 -0600

    Removed rattletrap locally
    Rattletrap will now always install itself to get the latest version

--- carball/rattletrap/check_rattletrap_version.py
+++ carball/rattletrap/check_rattletrap_version.py
@@ -1,26 +1,43 @@
+import inspect
 import os
 from urllib import request
 import json
+import fnmatch
 from distutils.version import StrictVersion
 
-response = request.urlopen('https://api.github.com/repos/tfausak/rattletrap/releases/latest')
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+path = os.path.dirname(os.path.abspath(filename))
 
-js = json.loads(response.read())
+def update_rattletrap():
+    print('updating rattletrap in path', path)
+    files = os.listdir(path)
+    print('files in path', files)
 
-cur_ver = '0.0.0'
-binaries = [f for f in os.listdir('.') if not f.endswith('.py') and os.path.isfile(f)]
-print (binaries)
-if len(binaries) > 0:
-    cur_ver = binaries[0].split('-')[1]
-update = StrictVersion(js['name']) > StrictVersion(cur_ver)
-print (f'GitHub version: {js["name"]}\n'
-       f'Current version: {cur_ver}\n'
-       f'Update? {update}')
-if update:
-    for file in binaries:
-        os.remove(file)
+    cur_ver = '0.0.0'
+    binaries = [f for f in files if not f.endswith('.py') and fnmatch.fnmatch(f, "*rattletrap*")]
+    print('existing found', binaries)
 
-    for asset in js['assets']:
+    response = request.urlopen('https://api.github.com/repos/tfausak/rattletrap/releases/latest')
 
-        print('Downloading {}'.format(asset['name']))
-        request.urlretrieve(asset['browser_download_url'], asset['name'])
+    js = json.loads(response.read())
+
+    if len(binaries) > 0:
+        cur_ver = binaries[0].split('-')[1]
+    update = StrictVersion(js['name']) > StrictVersion(cur_ver)
+    print (f'GitHub version: {js["name"]}\n'
+           f'Current version: {cur_ver}\n'
+           f'Update? {update}')
+    if update:
+        for file in binaries:
+            os.remove(file)
+
+        for asset in js['assets']:
+            print('Downloading {}'.format(asset['name']))
+            new_file = os.path.join(path, asset['name'])
+            request.urlretrieve(asset['browser_download_url'], filename=new_file)
+            print('making file executable', new_file)
+            os.chmod(new_file, 0o777)
+
+
+if __name__ == "__main__":
+    update_rattletrap()
--- init.py
+++ init.py
@@ -1,7 +1,12 @@
-from utils.create_proto import create_proto_files
-from utils.import_fixer import convert_to_relative_imports
+def initialize_project():
+    from rattletrap.check_rattletrap_version import update_rattletrap
+    from utils.create_proto import create_proto_files
+    from utils.import_fixer import convert_to_relative_imports
+
+    create_proto_files()
+    convert_to_relative_imports()
+    update_rattletrap()
 
 
 if __name__ == "__main__":
-    create_proto_files()
-    convert_to_relative_imports()
+    initialize_project()
