fa216bd94a2e3f93552ac0be790a1c5393cd1348
dtracers <dtracers@gmail.com>
2019-05-03 15:27:23 -0600

    update rattletrap after protobuf has been added.

--- init.py
+++ init.py
@@ -1,12 +1,14 @@
-def initialize_project():
+def initalize_rattletrap():
     from carball.rattletrap.check_rattletrap_version import update_rattletrap
-    from utils.create_proto import create_proto_files
-    from utils.import_fixer import convert_to_relative_imports
-
-    create_proto_files()
-    convert_to_relative_imports()
     update_rattletrap()
 
+def initialize_project():
+    from utils.create_proto import create_proto_files
+    from utils.import_fixer import convert_to_relative_imports
+    
+    create_proto_files()
+    convert_to_relative_imports()
+    initalize_rattletrap()
 
 if __name__ == "__main__":
     initialize_project()
