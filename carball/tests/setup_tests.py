f24812a4e262c42d9ab1f07a8eb10382ec93e9f5
dtracers <dtracers@gmail.com>
2019-05-04 11:22:01 -0600

    added test to cover version checker

--- carball/tests/setup_tests.py
+++ carball/tests/setup_tests.py
@@ -3,6 +3,7 @@ import unittest
 
 from carball.rattletrap.rattletrap_utils import get_rattletrap_binaries, get_rattletrap_path, download_rattletrap
 from carball.rattletrap.run_rattletrap import create_rattletrap_command
+from carball.rattletrap.check_rattletrap_version import update_rattletrap
 
 
 class setup_tests(unittest.TestCase):
@@ -24,3 +25,10 @@ class setup_tests(unittest.TestCase):
         self.cleanup()
 
         create_rattletrap_command('file.replay', 'outputdir')
+
+    def test_direct_check_version(self):
+        self.cleanup()
+        update_rattletrap()
+
+        #skip update
+        update_rattletrap()
