15044d8a05cf901aba5f70849d114963e1f1550f
dtracers <dtracers@gmail.com>
2019-05-03 23:26:05 -0600

    Added typing

--- carball/rattletrap/rattletrap_utils.py
+++ carball/rattletrap/rattletrap_utils.py
@@ -1,15 +1,16 @@
 import inspect
 import os
 import fnmatch
+from typing import List, Union
 
 
-def get_rattletrap_path():
+def get_rattletrap_path() -> Union[bytes, str]:
     filename = inspect.getframeinfo(inspect.currentframe()).filename
     path = os.path.dirname(os.path.abspath(filename))
     return path
 
 
-def get_rattletrap_binaries(path):
+def get_rattletrap_binaries(path: Union[bytes, str]) -> List[Union[bytes, str]]:
     files = os.listdir(path)
     binaries = [f for f in files if not f.endswith('.py') and fnmatch.fnmatch(f, "*rattletrap*")]
     print(binaries)
