43f534778381d56433dd5d73b5aa57f3cd08b9a0
Sciguymjm <sciguymjm@gmail.com>
2019-05-07 12:21:51 -0400

    Custom rattletrap location

--- carball/decompile_replays.py
+++ carball/decompile_replays.py
@@ -1,33 +1,32 @@
-import os
 import logging
-import platform as pt
+import os
 
 from carball.analysis.analysis_manager import AnalysisManager
-from carball.extras.per_goal_analysis import PerGoalAnalysis
-from carball.json_parser.sanity_check.sanity_check import SanityChecker
-from carball.json_parser.game import Game
 from carball.controls.controls import ControlsCreator
+from carball.extras.per_goal_analysis import PerGoalAnalysis
+from carball.json_parser.game import Game
+from carball.json_parser.sanity_check.sanity_check import SanityChecker
 from carball.rattletrap import run_rattletrap
-
 
 logger = logging.getLogger(__name__)
 BASE_DIR = os.path.dirname(__file__)
 
 
-def decompile_replay(replay_path, output_path: str=None, overwrite=True):
+def decompile_replay(replay_path, output_path: str = None, overwrite: bool = True, rattletrap_path: str = None):
     """
     Takes a path to the replay and outputs the json of that replay.
 
     :param replay_path: Path to a specific replay.
     :param output_path: The output path of rattletrap.
     :param overwrite: True if we should recreate the json even if it already exists.
+    :param rattletrap_path: Custom location for rattletrap executable. Path to folder.
     :return: The json created from rattle trap.
     """
-    return run_rattletrap.decompile_replay(replay_path, output_path)
+    return run_rattletrap.decompile_replay(replay_path, output_path, overwrite, rattletrap_path)
 
 
-def analyze_replay_file(replay_path: str, output_path: str=None, overwrite=True, controls: ControlsCreator=None,
-                        sanity_check: SanityChecker=None, analysis_per_goal=False):
+def analyze_replay_file(replay_path: str, output_path: str = None, overwrite=True, controls: ControlsCreator = None,
+                        sanity_check: SanityChecker = None, analysis_per_goal=False):
     """
     Decompile and analyze a replay file.
 
@@ -59,4 +58,5 @@ def analyze_replay_file(replay_path: str, output_path: str=None, overwrite=True,
 
 if __name__ == '__main__':
     from carball.tests.analysis_test import __test_replays
+
     __test_replays(BASE_DIR)
