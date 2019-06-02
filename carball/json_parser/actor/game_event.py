eb57c8cc456f4e3253937ddd65a9fd9ef545d723
Long Tran <lngtrn94@gmail.com>
2019-06-02 07:51:49 +0200

    Refactor actor parsing (#134)
    * start refactoring json parser, game info handler
    
    * add game event handler
    
    * add ball handler
    
    * add player and team handler
    
    * add car handler
    
    * apply actor handlers based on priority
    
    * add jump, double jump and dodge handler
    
    * add boost handlers
    
    * add camera settings handler
    
    * add rumble handler
    
    * add rumble time till item
    
    * back to frame_number indexing
    
    * fully use the frame parser
    
    * clean up game
    
    * add comments
    
    * skip frames with a delta of 0
    
    * remove obselete todo
    
    * remove unused class attribute
    
    * class attribute documentation

---
+++ carball/json_parser/actor/game_event.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+class GameEventHandler(BaseActorHandler):
+
+    @classmethod
+    def can_handle(cls, actor: dict) -> bool:
+        return actor['ClassName'].startswith('TAGame.GameEvent_Soccar_TA')
+
+    def update(self, actor: dict, frame_number: int, time: float, delta: float) -> None:
+        self.parser.soccar_game_event_actor = actor
+        frame_data = {
+            'time': time,
+            'delta': delta,
+            'seconds_remaining': actor.get('TAGame.GameEvent_Soccar_TA:SecondsRemaining', None),
+            'replicated_seconds_remaining': actor.get('TAGame.GameEvent_TA:ReplicatedGameStateTimeRemaining', None),
+            'is_overtime': actor.get('TAGame.GameEvent_Soccar_TA:bOverTime', None),
+            'ball_has_been_hit': actor.get('TAGame.GameEvent_Soccar_TA:bBallHasBeenHit', None)
+        }
+        self.parser.frames_data[frame_number] = frame_data
---
+++ carball/json_parser/actor/game_info.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+class GameInfoHandler(BaseActorHandler):
+    priority = 1
+
+    @classmethod
+    def can_handle(cls, actor: dict) -> bool:
+        return actor['TypeName'].endswith(':GameReplicationInfoArchetype')
+
+    def update(self, actor: dict, frame_number: int, time: float, delta: float) -> None:
+        if self.parser.game_info_actor is None:
+            self.parser.game_info_actor = actor
