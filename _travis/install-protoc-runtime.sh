6f47510398c4d799eb393cd5173ea23a7cccf43e
dtracers <dtracers@gmail.com>
2019-06-06 23:43:44 -0600

    remove check

--- _travis/install-protoc-runtime.sh
+++ _travis/install-protoc-runtime.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 protoc_version=$1
+
 wget https://github.com/protocolbuffers/protobuf/archive/v${protoc_version}.zip
 unzip v${protoc_version}.zip
 cd v${protoc_version}
 ./configure
 make
-make check
 make install
 ldconfig
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
