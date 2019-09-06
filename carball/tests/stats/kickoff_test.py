39dd27a47980e47727afbaa6beb1409a060354d3
dtracers <dtracers@gmail.com>
2019-09-05 23:12:24 -0600

    removed non submission code

--- carball/tests/stats/kickoff_test.py
+++ carball/tests/stats/kickoff_test.py
@@ -46,3 +46,54 @@ class Test_Kickoff():
             assert kickoff_stats.total_kickoffs == 2
 
         run_analysis_test_on_replay(test, get_raw_replays()["LAST_KICKOFF_NO_TOUCH"])
+
+    def test_all_kickoffs(self):
+        def test(analysis: AnalysisManager):
+
+            proto_game = analysis.get_protobuf_data()
+            kickoffs = proto_game.game_stats.kickoffs
+            assert (len(kickoffs) == 5)
+
+            first_player = proto_game.players[0].stats
+            kickoff_stats = first_player.kickoff_stats
+            assert kickoff_stats.num_time_first_touch == 0
+            assert kickoff_stats.num_time_go_to_ball == 2
+            assert kickoff_stats.num_time_boost == 1
+            assert kickoff_stats.num_time_cheat == 1
+            assert kickoff_stats.num_time_defend == 1
+            assert kickoff_stats.total_kickoffs == 5
+
+            second_player_stats = proto_game.players[1].stats.kickoff_stats
+            assert second_player_stats.num_time_first_touch == 5
+            assert second_player_stats.num_time_go_to_ball == 5
+
+        run_analysis_test_on_replay(test, get_raw_replays()["5_DIVERSE_KICKOFFS"])
+
+    def test_all_kickoffs_2(self):
+        def test(analysis: AnalysisManager):
+
+            proto_game = analysis.get_protobuf_data()
+            kickoffs = proto_game.game_stats.kickoffs
+            assert (len(kickoffs) == 6)
+
+            first_player = proto_game.players[0].stats
+            kickoff_stats = first_player.kickoff_stats
+            assert kickoff_stats.num_time_first_touch == 1
+            assert kickoff_stats.num_time_go_to_ball == 1
+            assert kickoff_stats.num_time_boost == 1
+            assert kickoff_stats.num_time_cheat == 0
+            assert kickoff_stats.num_time_defend == 1
+            assert kickoff_stats.num_time_afk == 1
+            assert kickoff_stats.total_kickoffs == 6
+            assert 2 == (kickoff_stats.total_kickoffs -
+                         (kickoff_stats.num_time_go_to_ball +
+                          kickoff_stats.num_time_boost +
+                          kickoff_stats.num_time_cheat +
+                          kickoff_stats.num_time_defend +
+                          kickoff_stats.num_time_afk))
+
+            second_player_stats = proto_game.players[1].stats.kickoff_stats
+            assert second_player_stats.num_time_first_touch == 5
+            assert second_player_stats.num_time_go_to_ball == 6
+
+        run_analysis_test_on_replay(test, get_raw_replays()["6_DIVERSE_KICKOFFS"])
