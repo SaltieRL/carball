0e9bd5bd3cea4a5334f2da334eea695f335235c6
dtracers <dtracers@gmail.com>
2019-09-05 22:35:36 -0600

    Added player kickoff totals

--- carball/tests/stats/kickoff_test.py
+++ carball/tests/stats/kickoff_test.py
@@ -6,12 +6,15 @@ from carball.tests.utils import run_analysis_test_on_replay, get_raw_replays
 class Test_Kickoff():
 
     def test_0_kickoffs(self):
-
         def test(analysis: AnalysisManager):
 
             proto_game = analysis.get_protobuf_data()
             kickoffs = proto_game.game_stats.kickoffs
             assert (len(kickoffs) == 0)
+
+            first_player = proto_game.players[0].stats
+            kickoff_stats = first_player.kickoff_stats
+            assert kickoff_stats.total_kickoffs == 0
 
         run_analysis_test_on_replay(test, get_raw_replays()["KICKOFF_NO_TOUCH"])
 
@@ -22,6 +25,12 @@ class Test_Kickoff():
             kickoffs = proto_game.game_stats.kickoffs
             assert (len(kickoffs) == 3)
 
+            first_player = proto_game.players[0].stats
+            kickoff_stats = first_player.kickoff_stats
+            assert kickoff_stats.num_time_first_touch == 3
+            assert kickoff_stats.num_time_go_to_ball == 3
+            assert kickoff_stats.total_kickoffs == 3
+
         run_analysis_test_on_replay(test, get_raw_replays()["3_KICKOFFS"])
 
     def test_last_kickoff_no_touch(self):
@@ -31,5 +40,9 @@ class Test_Kickoff():
             proto_game = analysis.get_protobuf_data()
             kickoffs = proto_game.game_stats.kickoffs
             assert (len(kickoffs) == 2)
+
+            first_player = proto_game.players[0].stats
+            kickoff_stats = first_player.kickoff_stats
+            assert kickoff_stats.total_kickoffs == 2
 
         run_analysis_test_on_replay(test, get_raw_replays()["LAST_KICKOFF_NO_TOUCH"])
