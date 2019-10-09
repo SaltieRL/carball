2d1e1c099afa43f7b521033a72e239b6fd85bb25
dtracers <dtracers@gmail.com>
2019-10-09 00:07:02 -0600

    fixed some tests that were not running correctly
    should improve code coverage a tiny bit

---
+++ carball/tests/metadata/command_line_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+from carball.command_line import main
+from carball.rattletrap.run_rattletrap import RattleTrapException
+from carball.tests.utils import get_replay_path, get_raw_replays
+
+
+class Test_Commandline():
+    def test_command_line_valid_input(self):
+        replay_path = get_replay_path(get_raw_replays()["KICKOFF_NO_TOUCH"][0])
+        main(program_args=["--i", replay_path, "-v", "--dry-run"])
+
+    def test_command_line_invalid_input(self):
+        replay_path = get_replay_path("INVALID_REPLAY")
+        with pytest.raises(RattleTrapException):
+            main(program_args=["--i", replay_path, "-s", "--proto", "PATH_DOESNT_MATTER"])
+
+    def test_command_line_no_output(self):
+        replay_path = get_replay_path(get_raw_replays()["KICKOFF_NO_TOUCH"][0])
+        with pytest.raises(SystemExit):
+            main(program_args=["--i", replay_path])
