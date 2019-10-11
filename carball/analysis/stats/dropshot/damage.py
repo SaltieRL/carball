ba6827152116c5d42aa578e92afeb9ceff1a1a27
dtracers <dtracers@gmail.com>
2019-10-11 16:02:51 -0600

    fix travis error

--- carball/analysis/stats/dropshot/damage.py
+++ carball/analysis/stats/dropshot/damage.py
@@ -6,7 +6,6 @@ import pandas as pd
 from carball.analysis.stats.stats import BaseStat
 from carball.generated.api.player_pb2 import Player
 from carball.generated.api import game_pb2
-from carball.generated.api.stats.dropshot_pb2 import DropshotStats
 from carball.generated.api.stats.player_stats_pb2 import PlayerStats
 from carball.generated.api.stats.team_stats_pb2 import TeamStats
 from carball.json_parser.game import Game
@@ -41,7 +40,7 @@ class DropshotStats(BaseStat):
             player_stats[player_id]['total'] += len(event['tiles'])
             player_stats[player_id]['max'] += max_dmg
 
-        self.apply_damage_stats(player_stats)
+        self.apply_damage_stats(player_stat_map, player_stats)
 
     def calculate_team_stat(self, team_stat_list: Dict[int, TeamStats], game: Game, proto_game: game_pb2.Game,
                             player_map: Dict[str, Player], data_frame: pd.DataFrame):
@@ -65,7 +64,7 @@ class DropshotStats(BaseStat):
             team_stats[team]['total'] += len(event['tiles'])
             team_stats[team]['max'] += max_dmg
 
-        self.apply_damage_stats(team_stat_list)
+        self.apply_damage_stats(team_stat_list, team_stats)
 
     def calculate_stat(self, proto_stat, game: Game, proto_game: game_pb2.Game, player_map: Dict[str, Player],
                        data_frame: pd.DataFrame):
@@ -97,8 +96,8 @@ class DropshotStats(BaseStat):
         proto_game.game_stats.dropshot_stats.tile_stats.damaged_tiles = damaged
         proto_game.game_stats.dropshot_stats.tile_stats.destroyed_tiles = destroyed
 
-    def apply_damage_stats(self, game_stats):
-        for key, stats in game_stats.items():
+    def apply_damage_stats(self, stat_list, game_stats):
+        for key, stats in stat_list.items():
             stats.dropshot_stats.total_damage = game_stats[key]['total']
 
             if game_stats[key]['max'] > 0:
