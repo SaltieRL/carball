c59ffeeeb6f715297d9bd426f808580c8ab9505d
Long Tran <lngtrn94@gmail.com>
2019-10-12 20:57:54 +0200

    add numpy and explicit versions to dependencies

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     version=version_string,
     packages=setuptools.find_packages(),
     include_package_data=True,
-    install_requires=['pandas==0.24.2', 'protobuf==3.6.1', 'xlrd'],
+    install_requires=['pandas==0.24.2', 'protobuf==3.6.1', 'numpy==1.17.0'],
     url='https://github.com/SaltieRL/carball',
     keywords=['rocket-league'],
     license='Apache 2.0',
