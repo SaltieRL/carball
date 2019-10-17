71695f95bf42999be2e71be5b672124a5ec60018
dtracers <dtracers@gmail.com>
2019-10-16 21:40:27 -0600

    get largest file always.
    This should always grab the highest version file.  So it can grab the cloud parser if it is a higher custom version than what is released.

--- carball/rattletrap/rattletrap_utils.py
+++ carball/rattletrap/rattletrap_utils.py
@@ -2,6 +2,7 @@ import inspect
 import os
 import fnmatch
 import logging
+from distutils.version import StrictVersion
 from typing import List, Union
 
 log = logging.getLogger(__name__)
@@ -15,7 +16,15 @@ def get_rattletrap_path() -> Union[bytes, str]:
 
 def get_rattletrap_binaries(path: Union[bytes, str]) -> List[Union[bytes, str]]:
     files = os.listdir(path)
-    binaries = [f for f in files if not f.endswith('.py') and fnmatch.fnmatch(f, "*rattletrap*")]
+    binaries = [f for f in files if not f.endswith('.py') and (fnmatch.fnmatch(f, "*rattletrap*"))]
+    log.debug(f'Discovered rattletrap binaries: {binaries}')
+    return binaries
+
+
+def get_all_binaries(path: Union[bytes, str]) -> List[Union[bytes, str]]:
+    files = os.listdir(path)
+    binaries = [f for f in files if not f.endswith('.py') and (fnmatch.fnmatch(f, "*rattletrap*") or
+                                                               fnmatch.fnmatch(f, "*cloud_parser*"))]
     log.debug(f'Discovered rattletrap binaries: {binaries}')
     return binaries
 
@@ -25,13 +34,28 @@ def download_rattletrap():
     update_rattletrap()
 
 
+def get_binary_version(filename: str) -> StrictVersion:
+    try:
+        return StrictVersion(filename.split('-')[1])
+    except:
+        return None
+
+
+def get_highest_binary(binaries):
+    if len(binaries) == 1:
+        return binaries
+    return binaries.sort(key=lambda value: get_binary_version(str(value)))
+
+
 def get_binary_for_platform(platform, binaries):
     if platform == 'Windows':
-        binary = [f for f in binaries if f.endswith('.exe')][0]
+        binaries = [f for f in binaries if f.endswith('.exe')]
     elif platform == 'Linux':
-        binary = [f for f in binaries if 'linux' in f][0]
+        binaries = [f for f in binaries if 'linux' in f]
     elif platform == 'Darwin':
-        binary = [f for f in binaries if 'osx' in f][0]
+        binaries = [f for f in binaries if 'osx' in f]
     else:
         raise Exception('Unknown platform, unable to process replay file.')
-    return binary
+
+    get_highest_binary(binaries)
+    return binaries[-1]
