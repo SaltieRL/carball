43941d50ef9653e572ce70dffed1005d20f7d201
DivvyCr <DivvyCr@users.noreply.github.com>
2020-04-29 19:09:48 +0100

    Final touches (hopefully).

--- carball/tests/docs/data_frame_docs.py
+++ carball/tests/docs/data_frame_docs.py
@@ -10,14 +10,10 @@ def create_df_docs():
     replay_path = get_replay_path("SHORT_SAMPLE.replay")
 
     working_dir = os.path.dirname(__file__)
-    output_dir = os.path.join(working_dir, 'output')
 
     df_summary_path = os.path.join(working_dir, 'df_summary.md')
     df_methods_path = os.path.join(working_dir, 'df_methods.md')
-    df_docs_path = os.path.join(output_dir, "data_frame.md")
-
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+    df_docs_path = os.path.join(working_dir, "data_frame.md")
 
     analysis = analyze_replay_file(replay_path, clean=False)
 
