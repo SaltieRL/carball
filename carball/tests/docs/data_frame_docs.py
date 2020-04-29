ff1ab80e84b291cff4a8dba6427f01459539519d
dtracers <dtracers@gmail.com>
2020-04-29 11:57:18 -0700

    Create wiki.yml (#229)
    * Create wiki.yml
    
    Allow the creation of wiki files automatically for documentation purposes
    
    * list docs for compiling
    
    * runs python step
    
    * Changed to using relative imports
    
    * fix wiki error
    
    * setup protobuf inside the action
    
    * adding init.py
    
    * try installing via an action
    
    * the action does not work for us
    
    * adding logging
    
    * added new step
    
    * maybe dont copy everything
    
    * local copying
    
    * Update wiki.yml
    
    * Update wiki.yml
    
    * trying something
    
    * checking for protoc
    
    * sequential execution
    
    * sequential execution (2)
    
    * success condition + full path names
    
    * small syntax fix?
    
    * smaller fix
    
    * fix?
    
    * fix? (2)
    
    * Print dirs for debug.
    
    * Duplicate works!
    Sequential execution for Dependencies.
    
    * Duplicate didn't solve the problem. Checking logs w/o it.
    
    * Install wheel before protobuf setup.
    
    * Split-up for debugging.
    
    * Bunch of prints
    
    * Catch exception?
    
    * Full fix: added shared library refresh to install-protoc.sh, which supposedly fixes the .libs directory problem.
    
    NOTE: Currently not skipping df_methods/summary.md to see if anything uploads.
    
    * Using the output folder; reformatting files for proper markdown.
    
    * Final touches (hopefully).
    
    Co-authored-by: Matthew Mage <Sciguymjm@users.noreply.github.com>
    Co-authored-by: DivvyCr <DivvyCr@users.noreply.github.com>

--- carball/tests/docs/data_frame_docs.py
+++ carball/tests/docs/data_frame_docs.py
@@ -1,7 +1,7 @@
 import os
 
-import carball
-from carball.tests.utils import get_replay_path
+from ... import analyze_replay_file
+from ..utils import get_replay_path
 
 
 # All relevant files begin with df_ .
@@ -10,16 +10,12 @@ def create_df_docs():
     replay_path = get_replay_path("SHORT_SAMPLE.replay")
 
     working_dir = os.path.dirname(__file__)
-    output_dir = os.path.join(working_dir, 'output')
 
     df_summary_path = os.path.join(working_dir, 'df_summary.md')
     df_methods_path = os.path.join(working_dir, 'df_methods.md')
-    df_docs_path = os.path.join(output_dir, "DATA_FRAME_INFO.md")
+    df_docs_path = os.path.join(working_dir, "data_frame.md")
 
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    analysis = carball.analyze_replay_file(replay_path, clean=False)
+    analysis = analyze_replay_file(replay_path, clean=False)
 
     data_frame = analysis.get_data_frame()
 
@@ -38,7 +34,7 @@ def create_df_docs():
 
     df_docs.write("\n")
 
-    df_docs.write("##Example (list of columns)\n")
+    df_docs.write("## Example (list of columns)\n")
     df_docs.write("Each subheading is the primary column heading, and each row is the secondary column heading."
                   "The 'Player' subheading will be unique for each player in the replay (i.e. for a 3v3 game, there "
                   "will be 6 distinct players)\n")
@@ -47,7 +43,7 @@ def create_df_docs():
     for c in data_frame.columns:
         if current_heading != c[0]:
             current_heading = c[0]
-            df_docs.write("\n####" + c[0])
+            df_docs.write("\n#### " + c[0])
 
         df_docs.write("\n\t" + c[1])
 
