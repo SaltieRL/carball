f0cc71245c61ab92acf4f8b498c5c23185980286
DivvyCr <DivvyCr@users.noreply.github.com>
2020-04-30 19:53:36 +0100

    Final touches (still need to add dropshot, tho).

--- benchmarking/benchmarking.py
+++ benchmarking/benchmarking.py
@@ -1,36 +1,41 @@
-import os
-
+from carball.tests.utils import get_replay_path
 from carball.decompile_replays import analyze_replay_file
+
+"""
+This file is doing benchmarking for the main replay varieties. The tests are run via the benchmarking GitHub action.
+
+kwargs is a dict of variable name to value for the benchmarked method.
+rounds are collections of iterations, and the data is collected off of these.
+iterations are single method runs.
+"""
 
 
 def test_short_sample(benchmark):
-    carball_dir = os.path.dirname(os.path.dirname(__file__))
-    replay_path = os.path.join(carball_dir, "carball/tests/replays/SHORT_SAMPLE.replay")
+    replay_path = get_replay_path("SHORT_SAMPLE.replay")
 
     benchmark.pedantic(analyze_replay_file,
                        kwargs={"replay_path":replay_path}, rounds=50, iterations=3)
 
 
 def test_full_rumble(benchmark):
-    carball_dir = os.path.dirname(os.path.dirname(__file__))
-    replay_path = os.path.join(carball_dir, "carball/tests/replays/RUMBLE_FULL.replay")
+    replay_path = get_replay_path("RUMBLE_FULL.replay")
 
     benchmark.pedantic(analyze_replay_file,
                        kwargs={"replay_path":replay_path}, rounds=20, iterations=3)
 
 
 def test_oce_rlcs(benchmark):
-    carball_dir = os.path.dirname(os.path.dirname(__file__))
-    replay_path = os.path.join(carball_dir, "carball/tests/replays/OCE_RLCS_7_CARS.replay")
+    replay_path = get_replay_path("OCE_RLCS_7_CARS.replay")
 
     benchmark.pedantic(analyze_replay_file,
                        kwargs={"replay_path":replay_path}, rounds=20, iterations=3)
 
 
 def test_oce_rlcs_intensive(benchmark):
-    carball_dir = os.path.dirname(os.path.dirname(__file__))
-    replay_path = os.path.join(carball_dir, "carball/tests/replays/OCE_RLCS_7_CARS.replay")
+    replay_path = get_replay_path("OCE_RLCS_7_CARS.replay")
 
     benchmark.pedantic(analyze_replay_file,
                        kwargs={"replay_path":replay_path, "calculate_intensive_events":True}, rounds=5, iterations=3)
 
+
+# TODO Add a dropshot replay (full-game).
