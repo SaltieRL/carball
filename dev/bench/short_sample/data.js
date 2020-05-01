1aeb321c9af7b1d715fa588d310463384ac9ae63
github-action-benchmark <github@users.noreply.github.com>
2020-05-01 17:36:48 +0000

    add Carball Benchmarks short_sample (pytest) benchmark result for bcc1a8cee5f096035713ca264410c4d69dc08aec

---
+++ dev/bench/short_sample/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1588354608353,
+  "repoUrl": "https://github.com/SaltieRL/carball",
+  "entries": {
+    "Carball Benchmarks short_sample": [
+      {
+        "commit": {
+          "author": {
+            "email": "DivvyCr@users.noreply.github.com",
+            "name": "DivvyCr",
+            "username": "DivvyCr"
+          },
+          "committer": {
+            "email": "DivvyCr@users.noreply.github.com",
+            "name": "DivvyCr",
+            "username": "DivvyCr"
+          },
+          "distinct": true,
+          "id": "bcc1a8cee5f096035713ca264410c4d69dc08aec",
+          "message": "Final touches?",
+          "timestamp": "2020-05-01T18:28:05+01:00",
+          "tree_id": "9e35341b9c327b42b51b45f418e4f9065ce8fe35",
+          "url": "https://github.com/SaltieRL/carball/commit/bcc1a8cee5f096035713ca264410c4d69dc08aec"
+        },
+        "date": 1588354600711,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "carball/tests/benchmarking/benchmarking.py::test_short_sample",
+            "value": 0.7970441034287731,
+            "unit": "iter/sec",
+            "range": "stddev: 0.014330996697547907",
+            "extra": "mean: 1.2546357167666617 sec\nrounds: 10"
+          }
+        ]
+      }
+    ]
+  }
+}
