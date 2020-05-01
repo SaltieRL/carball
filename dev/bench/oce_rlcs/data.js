efea84727642d2c438c8ab77a51060ae215c578d
github-action-benchmark <github@users.noreply.github.com>
2020-05-01 17:46:56 +0000

    add Carball Benchmarks oce_rlcs (pytest) benchmark result for bcc1a8cee5f096035713ca264410c4d69dc08aec

---
+++ dev/bench/oce_rlcs/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1588355216598,
+  "repoUrl": "https://github.com/SaltieRL/carball",
+  "entries": {
+    "Carball Benchmarks oce_rlcs": [
+      {
+        "commit": {
+          "author": {
+            "email": "DivvyCr@users.noreply.github.com",
+            "name": "DivvyCr",
+            "username": "DivvyCr"
+          },
+          "committer": {
+            "email": "DivvyCr@users.noreply.github.com",
+            "name": "DivvyCr",
+            "username": "DivvyCr"
+          },
+          "distinct": true,
+          "id": "bcc1a8cee5f096035713ca264410c4d69dc08aec",
+          "message": "Final touches?",
+          "timestamp": "2020-05-01T18:28:05+01:00",
+          "tree_id": "9e35341b9c327b42b51b45f418e4f9065ce8fe35",
+          "url": "https://github.com/SaltieRL/carball/commit/bcc1a8cee5f096035713ca264410c4d69dc08aec"
+        },
+        "date": 1588355208822,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "carball/tests/benchmarking/benchmarking.py::test_oce_rlcs",
+            "value": 0.054604254911294156,
+            "unit": "iter/sec",
+            "range": "stddev: 0.19284145756466925",
+            "extra": "mean: 18.313591159233336 sec\nrounds: 10"
+          },
+          {
+            "name": "carball/tests/benchmarking/benchmarking.py::test_oce_rlcs_intensive",
+            "value": 0.04855320417160526,
+            "unit": "iter/sec",
+            "range": "stddev: 0.05870499237634016",
+            "extra": "mean: 20.595963069000028 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
