b413e2bb3e029248f0b9c17a678bc79031314d3c
github-action-benchmark <github@users.noreply.github.com>
2020-05-02 17:47:31 +0000

    add Carball Benchmarks short_dropshot (pytest) benchmark result for 454cfd65360a1ef1ed05fef48d8789b1e6e692db

--- dev/bench/short_dropshot/data.js
+++ dev/bench/short_dropshot/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1588354621025,
+  "lastUpdate": 1588441651682,
   "repoUrl": "https://github.com/SaltieRL/carball",
   "entries": {
     "Carball Benchmarks short_dropshot": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.03095164285962033",
             "extra": "mean: 1.7422020856666678 sec\nrounds: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "54956345+DivvyCr@users.noreply.github.com",
+            "name": "Divvy",
+            "username": "DivvyCr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "454cfd65360a1ef1ed05fef48d8789b1e6e692db",
+          "message": "Support benchmarking reports and lower the benching time. (#242)\n\n* Support benchmarking reports and lower the benching time.\r\n\r\n* Support benchmarking reports and lower the benching time.\r\n\r\n* Use appropriate GH token.\r\n\r\n* split up files to increase performance\r\n\r\n* Final touches?\r\n\r\n* Only push on master\r\n\r\nComment all the time\r\n\r\n* fix invalid file\r\n\r\n* only run oce_rlcs\r\n\r\nintensive is skipped\r\n\r\n* make it run intensive as a separate only test\r\n\r\n* switch to using gh edit token for now\r\n\r\n* Split benchmarking action into 2 - one comments on all pushes, and the second uploads benchmarking data from pushes to master.\r\n\r\n* try to use the intensive version\r\n\r\nCo-authored-by: DivvyCr <DivvyCr@users.noreply.github.com>\r\nCo-authored-by: dtracers <dtracers@gmail.com>",
+          "timestamp": "2020-05-02T10:39:36-07:00",
+          "tree_id": "9804315e3662313d233f94e5050e03bf136f404f",
+          "url": "https://github.com/SaltieRL/carball/commit/454cfd65360a1ef1ed05fef48d8789b1e6e692db"
+        },
+        "date": 1588441626475,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "carball/tests/benchmarking/benchmarking.py::test_short_dropshot",
+            "value": 0.5608396631341442,
+            "unit": "iter/sec",
+            "range": "stddev: 0.012052625398053523",
+            "extra": "mean: 1.7830407971 sec\nrounds: 10"
           }
         ]
       }
