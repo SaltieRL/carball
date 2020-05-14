47558272838ae79f4e4aace3e93b39317b0b5524
Long Tran <lngtrn94@gmail.com>
2020-05-14 16:22:17 +0200

    fix dataframe gzip export

--- carball/tests/export_test.py
+++ carball/tests/export_test.py
@@ -1,5 +1,6 @@
 from tempfile import NamedTemporaryFile
 
+import gzip
 import pytest
 
 from carball.analysis.analysis_manager import AnalysisManager
@@ -19,6 +20,14 @@ class TestExport:
         def test(analysis: AnalysisManager):
             with NamedTemporaryFile(mode='wb') as f:
                 analysis.write_proto_out_to_file(f)
+
+        run_analysis_test_on_replay(test, get_raw_replays()["DEFAULT_3_ON_3_AROUND_58_HITS"], cache=replay_cache)
+
+    def test_gzip_export(self, replay_cache):
+        def test(analysis: AnalysisManager):
+            with NamedTemporaryFile(mode='wb') as f:
+                gzip_file = gzip.GzipFile(mode='wb', fileobj=f)
+                analysis.write_pandas_out_to_file(gzip_file)
 
         run_analysis_test_on_replay(test, get_raw_replays()["DEFAULT_3_ON_3_AROUND_58_HITS"], cache=replay_cache)
 
