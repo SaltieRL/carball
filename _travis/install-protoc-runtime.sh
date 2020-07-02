2df78f3e0df6a5d4d12d8d8ac8c68801d45f37b0
DivvyCr <DivvyCr@users.noreply.github.com>
2020-07-02 14:57:33 +0100

    Set env vars before installing.

--- _travis/install-protoc-runtime.sh
+++ _travis/install-protoc-runtime.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 protoc_version=$1
 
-wget https://github.com/protocolbuffers/protobuf/archive/v${protoc_version}.zip
-unzip v${protoc_version}.zip
-cd v${protoc_version}
+wget https://github.com/protocolbuffers/protobuf/releases/protobuf-cpp-${protoc_version}.zip
+unzip protobuf-cpp-${protoc_version}.zip
+cd protobuf-cpp-${protoc_version}
 ./configure
 make
-make install
-ldconfig
+
+# Environment variables should be set _prior_ to installing the protobuf library.
+# https://developers.google.com/protocol-buffers/docs/reference/python-generated#cpp_impl
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+
+# Install the protobuf library.
+make install
+ldconfig
