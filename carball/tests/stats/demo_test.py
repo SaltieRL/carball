04044a178e9e3aa0b1139edfe6afc5c5b7edee5a
DivvyCr <divvycr@gmail.com>
2020-08-08 09:31:53 +0100

    Final touches. (Bumps -> Intensive, adapt testing, commenting unused code for codecov)

--- carball/tests/stats/demo_test.py
+++ carball/tests/stats/demo_test.py
@@ -4,7 +4,7 @@ from carball.analysis.analysis_manager import AnalysisManager
 
 
 class Test_Demos:
-    def test_calculate_demos_correctly(self, replay_cache):
+    def test_calculate_demos_as_bumps_correctly(self, replay_cache):
         def test(analysis: AnalysisManager):
             proto_game = analysis.get_protobuf_data()
             count_demo_bumps = 0
@@ -13,4 +13,6 @@ class Test_Demos:
                     count_demo_bumps += 1
             assert count_demo_bumps == 1
 
-        run_analysis_test_on_replay(test, get_raw_replays()["1_DEMO"], cache=replay_cache)
+        # Skip test cache since this test is calculating intensive events.
+        run_analysis_test_on_replay(test, get_raw_replays()["1_DEMO"],
+                                    calculate_intensive_events=True)
