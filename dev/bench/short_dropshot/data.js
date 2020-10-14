d26affff4887a59415e3b944082f0f3ab3c39193
github-action-benchmark <github@users.noreply.github.com>
2020-10-14 04:10:31 +0000

    add Carball Benchmarks short_dropshot (pytest) benchmark result for 3e66f175378f050b84bc9a3b15de0420505092a7

--- dev/bench/short_dropshot/data.js
+++ dev/bench/short_dropshot/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1601705487200,
+  "lastUpdate": 1602648629751,
   "repoUrl": "https://github.com/SaltieRL/carball",
   "entries": {
     "Carball Benchmarks short_dropshot": [
@@ -372,6 +372,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.03759415267222453",
             "extra": "mean: 1.5210779702666741 sec\nrounds: 10"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Sciguymjm@users.noreply.github.com",
+            "name": "Matthew Mage",
+            "username": "Sciguymjm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3e66f175378f050b84bc9a3b15de0420505092a7",
+          "message": "Add ballcam/handbrake times (#259)\n\n* Add ballcam/handbrake times\r\n\r\n* Remove comments",
+          "timestamp": "2020-10-13T21:03:13-07:00",
+          "tree_id": "7ea5d0947c001205584a02288cb924d8326ab306",
+          "url": "https://github.com/SaltieRL/carball/commit/3e66f175378f050b84bc9a3b15de0420505092a7"
+        },
+        "date": 1602648611144,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "carball/tests/benchmarking/benchmarking.py::test_short_dropshot",
+            "value": 0.6936401698877517,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0052686922532796596",
+            "extra": "mean: 1.441669677466668 sec\nrounds: 10"
           }
         ]
       }
